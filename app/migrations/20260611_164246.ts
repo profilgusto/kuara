bab6f3f926d3cb16d5ff7804bd90690a7f60a225
Filipe Rocha <filipe@ufsj.edu.br>
2026-06-11 13:43:04 -0300

    created db migration file

---
+++ app/migrations/20260611_164246.ts
@@ -0,0 +1,13 @@
+import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres'
+
+export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
+  await db.execute(sql`
+   ALTER TABLE "modules" ADD COLUMN "question_offsets" jsonb;
+  ALTER TABLE "_modules_v" ADD COLUMN "version_question_offsets" jsonb;`)
+}
+
+export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
+  await db.execute(sql`
+   ALTER TABLE "modules" DROP COLUMN "question_offsets";
+  ALTER TABLE "_modules_v" DROP COLUMN "version_question_offsets";`)
+}
--- app/migrations/index.ts
+++ app/migrations/index.ts
@@ -5,6 +5,7 @@ import * as migration_20260407_171654 from './20260407_171654';
 import * as migration_20260407_171744 from './20260407_171744';
 import * as migration_20260407_173934 from './20260407_173934';
 import * as migration_20260611_000000 from './20260611_000000';
+import * as migration_20260611_164246 from './20260611_164246';
 
 export const migrations = [
   {
@@ -41,5 +42,10 @@ export const migrations = [
     up: migration_20260611_000000.up,
     down: migration_20260611_000000.down,
     name: '20260611_000000',
+  },
+  {
+    up: migration_20260611_164246.up,
+    down: migration_20260611_164246.down,
+    name: '20260611_164246'
   },
 ];
